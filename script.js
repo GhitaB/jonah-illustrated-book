b9bf378b2b71c236ca855d34dd96b89f3fa1dd26
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-07-22 09:53:37 +0300

    Use AngularJS for gallery.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// alert("Script here.");
+angular.module('galleryApp', [])
+  .controller('galleryController', function() {
+    var gallery = this;
+    gallery.items = [
+      {
+        id: 1,
+        img: '01.png',
+        done: 80,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      },
+      {
+        id: 2,
+        img: '02.png',
+        done: 60,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      },
+    ];
+  });
