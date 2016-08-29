e6f5ab4fe80cb9ccc092ebc3e315f0293034d4bb
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-08-29 21:00:06 +0300

    Update worked hours.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ angular.module('galleryApp', [])
     gallery.language = "en";
     gallery.work_start_date = new Date(2016,05,20);
 
-    gallery.worked_hours = 71;  // EDIT HERE
+    gallery.worked_hours = 72;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
