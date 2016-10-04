e3bc4584e4e718456bb42248b2ab61856562c2d6
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-10-04 13:56:30 +0300

    More life in 03.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ angular.module('galleryApp', [])
     gallery.language = "ro";
     gallery.work_start_date = new Date(2016,05,20);
 
-    gallery.worked_hours = 101.5;  // EDIT HERE
+    gallery.worked_hours = 102;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
