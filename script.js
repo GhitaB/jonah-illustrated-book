f95ba95cd0625fd986ebc991caf03fa43e83df46
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-10-10 08:04:32 +0300

    Administration time. Trying to have friends on DeviantArt.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ angular.module('galleryApp', [])
     gallery.language = "ro";
     gallery.work_start_date = new Date(2016,05,20);
 
-    gallery.worked_hours = 114.5;  // EDIT HERE
+    gallery.worked_hours = 115;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
