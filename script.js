e6241e20ac723f8e8e5c14773b87cac6f22dfd8f
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-10-20 06:38:41 +0300

    Update hours. Fix typo.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ angular.module('galleryApp', [])
     gallery.language = "ro";
     gallery.work_start_date = new Date(2016,05,20);
 
-    gallery.worked_hours = 122.5;  // EDIT HERE
+    gallery.worked_hours = 123;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -389,9 +389,9 @@ angular.module('galleryApp', [])
         }
       },
       {
-        id: 90,
+        id: 30,
         img: '30.png',
-        done: 70,
+        done: 90,
         text: {
           ro: "2:8. Cei ce se lipesc de idoli deşerţi îndepărtează îndurarea de la ei.",
           en: "2:8. They that observe lying vanities forsake their own mercy."
