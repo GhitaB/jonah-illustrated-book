d456d47be621d2efe6cb7f26070b2b0f77bc835e
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-02-08 21:30:48 +0200

    Update 07.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ angular.module('galleryApp', [])
     gallery.language = "ro";
     gallery.work_start_date = new Date(2016,05,20);
 
-    gallery.worked_hours = 135;  // EDIT HERE
+    gallery.worked_hours = 135.5;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -179,7 +179,7 @@ angular.module('galleryApp', [])
       {
         id: 7,
         img: '07.png',
-        done: 40,
+        done: 60,
         text: {
           ro: "1:5A. Corăbierii s-au temut, au strigat fiecare la dumnezeul lui",
           en: "1:5A. Then the mariners were afraid, and cried every man unto his god, "
