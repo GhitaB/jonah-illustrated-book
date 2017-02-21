adc6a6ab05b32b45f840b6562a1d45c2f7d57208
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-02-21 21:58:35 +0200

    Add pages 1, 2.

--- script.js
+++ script.js
@@ -134,34 +134,16 @@ angular.module('galleryApp', [])
       {
         id: 1,
         img: '01.png',
-        done: 100,
+        done: 70,
         text: {
-          ro: "1:1. Cuvântul Domnului a vorbit lui Iona, fiul lui Amitai, astfel:",
-          en: "1:1. Now the word of the LORD came unto Jonah the son of Amittai, saying,"
+          ro: "1:1. Cuvântul Domnului a vorbit lui Iona, fiul lui Amitai, astfel: 1:2. \"Scoală-te, du-te la Ninive, cetatea cea mare, şi strigă împotriva ei. Căci răutatea ei s-a suit până la Mine!\" 1:3A. Şi Iona s-a sculat să fugă la Tars, departe de faţa Domnului.",
+          en: "1:1. Now the word of the LORD came unto Jonah the son of Amittai, saying, 1:2. Arise, go to Nineveh, that great city, and cry against it; for their wickedness is come up before me. 1:3A. But Jonah rose up to flee unto Tarshish from the presence of the LORD,"
         }
       },
       {
         id: 2,
         img: '02.png',
-        done: 100,
-        text: {
-          ro: "1:2. \"Scoală-te, du-te la Ninive, cetatea cea mare, şi strigă împotriva ei. Căci răutatea ei s-a suit până la Mine!\"",
-          en: "1:2. Arise, go to Nineveh, that great city, and cry against it; for their wickedness is come up before me."
-        }
-      },
-      {
-        id: 3,
-        img: '03.png',
-        done: 100,
-        text: {
-          ro: "1:3A. Şi Iona s-a sculat să fugă la Tars, departe de faţa Domnului.",
-          en: "1:3A. But Jonah rose up to flee unto Tarshish from the presence of the LORD,"
-        }
-      },
-      {
-        id: 4,
-        img: '04.png',
-        done: 100,
+        done: 70,
         text: {
           ro: "1:3B. S-a coborât la Iafo şi a găsit acolo o corabie care mergea la Tars.",
           en: "1:3B. and went down to Joppa; and he found a ship going to Tarshish:"
